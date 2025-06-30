7995cccfda106a66b0f023e38ee176726474ae46
misebox <misebox@gmail.com>
2025-07-01 08:31:31 +0900

    fix psql access

--- psql-local.sh
+++ psql-local.sh
@@ -3,4 +3,4 @@
 # PostgreSQL connection script for local development
 # This connects to the PostgreSQL instance defined in compose.yaml
 
-PGPASSWORD=pass psql -h localhost -p 5432 -U user -d dbname
+PGPASSWORD=password psql -h localhost -p 5432 -U postgres -d postgres
